d34a4aff9bd8d9570655831e7973f7bc550e300a
azzanco-dev <azani.erp@gmail.com>
2026-08-22 10:55:10 +0300

    Updated MovementDetail.tsx

--- src/screens/MovementDetail.tsx
+++ src/screens/MovementDetail.tsx
@@ -205,9 +205,11 @@ export function MovementDetail({ movementId, onBack, onNavigateMovement }: Movem
     if (linkedLog.movement_type === 'entry') {
       console.log('pre is entry')
       durationMs = new Date(linkedLog.recorded_at).getTime() - new Date(log.recorded_at).getTime();
+      console.log('entry', durationMs)
     } else if (linkedLog.movement_type === 'exit') {
       console.log('pre is exit')
       durationMs = new Date(log.recorded_at).getTime() - new Date(linkedLog.recorded_at).getTime();
+      console.log('exit', durationMs)
     }
   }
 
